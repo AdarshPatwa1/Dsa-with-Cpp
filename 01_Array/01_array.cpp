a0f3db634352a9f711a73aacb93babe25f8913bf
AdarshPatwa1 <adarshpatwaa@gmail.com>
2024-09-03 08:23:45 +0000

    Array datstructures

--- 01_Array/01_array.cpp
+++ 01_Array/01_array.cpp
@@ -7,97 +7,113 @@ class Array{
     int lastindex;
     int *ptr;
     public:
-    
-    Array(int size){
+    Array(int);
+    bool isempty();
+    void append(int);
+    void printarray();
+    void insert(int,int);
+    void edit(int,int);
+    void isdelete(int);
+    bool full();
+    int get(int);
+    int count();
+    ~Array();
+    int find(int);
+};  
+
+Array::Array(int size){
         lastindex=-1;
         capacity=size;
         ptr=new int[capacity];
-    }
+}
     
-    bool isempty(){
+bool Array::isempty(){
         return lastindex==-1;
-    }
+}
     
-    void append(int x){
-        if(lastindex==capacity-1){
-            cout<<"array is full";
+void Array::append(int x){
+    if(lastindex==capacity-1){
+        cout<<"array is full";
         }
-        else{
+    else{
         lastindex++;
         ptr[lastindex]=x;
         }
-    }
+}
     
-    void printarray(){
-        for(int i=0;i<=lastindex;i++){
-            cout<<ptr[i];
+void Array::printarray(){
+    for(int i=0;i<=lastindex;i++){
+        cout<<ptr[i];
             
-        }
-        
-    }
+        }    
+}
     
-    void insert(int index,int value){
-        int i;
-        if(index<0 || lastindex==capacity-1){
+void Array::insert(int index,int value){
+    int i;
+    if(index<0 || lastindex==capacity-1){
             cout<<"array is full";
         }
-        else if(index>lastindex+1){
+    else if(index>lastindex+1){
             cout<<"invalid index";
         }
-        else{
+    else{
         for(int i=lastindex;i>=index;i--){
             ptr[i+1]=ptr[i];
             
         }
-        ptr[index]=value;
-        lastindex++;
+    ptr[index]=value;
+    lastindex++;
         }
-    }
+}
     
-    void edit(int index,int value){
-        if(index>=0 && index<=lastindex){
+void Array::edit(int index,int value){
+    if(index>=0 && index<=lastindex){
         ptr[index]=value;
         }
-    }
+}
     
-    void isdelete(int index){
-        if(isempty()){
+void Array::isdelete(int index){
+    if(isempty()){
             cout<<"array is empty";
-        }else if(index<0 || index>lastindex+1){
+        }
+    else if(index<0 || index>lastindex+1){
             cout<<"invalid index";
         }
-        for(int i=index;i<lastindex;i++){
+    for(int i=index;i<lastindex;i++){
         ptr[i]=ptr[i+1];
-    }
+        }
     lastindex--;
-    }
+}
     
-    bool full(){
-        return lastindex==capacity-1;
-    }
+bool Array::full(){
+    return lastindex==capacity-1;
+}
     
-    int get(int index){
-        if(index>=0 && index<=lastindex){
+int Array::get(int index){
+    if(index>=0 && index<=lastindex){
             return ptr[index];
         }
-        cout<<"invalid index or empty";
-        return -1;
-    }
-    int count(){
-        return lastindex+1;
-    }
-    ~Array(){
-        delete[]ptr;
-    }
-    int find(int value){
-        for(int i=0;i<=lastindex;i++){
-            if(ptr[i]==value);{
-                return i;
-            }
+    cout<<"invalid index or empty";
+    return -1;
+}
+
+int Array::count(){
+    return lastindex+1;
+}
+
+Array::~Array(){
+    delete[]ptr;
+}
+
+int Array::find(int value){
+    for(int i=0;i<=lastindex;i++){
+        if(ptr[i]==value);{
+            return i;
         }
-        return -1;
     }
-};
+    return -1;
+}
+
 int main() {
     Array a(5);
     a.append(3);
